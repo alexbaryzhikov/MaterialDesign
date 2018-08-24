f9ef2cb4dfa48ae2f812e7645d472478d5721ea6
Alex Baryzhikov <aleksiarts@gmail.com>
2018-08-24 18:38:44 +0300

    AspectRatioImage refactor: Better initialization, attributes names

--- AspectRatioImage/app/src/main/java/com/alexbaryzhikov/aspectratioimage/AspectImageView.kt
+++ AspectRatioImage/app/src/main/java/com/alexbaryzhikov/aspectratioimage/AspectImageView.kt
@@ -4,24 +4,21 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.ImageView
+import kotlin.math.roundToInt
 
 class AspectImageView(context: Context, attrs: AttributeSet) : ImageView(context, attrs) {
 
-  private var widthRatio = 1
-  private var heightRatio = 1
-
-  init {
-    context.theme.obtainStyledAttributes(
-        attrs, R.styleable.AspectImageView, 0, 0).apply {
-      widthRatio = getInteger(R.styleable.AspectImageView_widthRatio, 1)
-      heightRatio = getInteger(R.styleable.AspectImageView_heightRatio, 1)
-    }
-  }
+  private val ratio = {
+    val styledAttrs = context.theme.obtainStyledAttributes(attrs, R.styleable.AspectImageView, 0, 0)
+    val w = styledAttrs.getInteger(R.styleable.AspectImageView_ratioX, 1)
+    val h = styledAttrs.getInteger(R.styleable.AspectImageView_ratioY, 1)
+    styledAttrs.recycle()
+    h / w.toDouble()
+  }.invoke()
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    val threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) * heightRatio / widthRatio
-    val threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY)
-    Log.i(javaClass.simpleName, "${MeasureSpec.getSize(widthMeasureSpec)} x $threeTwoHeight")
-    super.onMeasure(widthMeasureSpec, threeTwoHeightSpec)
+    val height = (MeasureSpec.getSize(widthMeasureSpec) * ratio).roundToInt()
+    Log.i(javaClass.simpleName, "${MeasureSpec.getSize(widthMeasureSpec)} : $height")
+    super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY))
   }
 }
